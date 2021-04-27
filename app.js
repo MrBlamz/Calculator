ebee56127974586c134e53c71ef98f39dda6aadf
Pedro Ribeiro <pedro.miguel.r1@hotmail.com>
2021-04-27 15:34:10 +0100

    Add function that returns the operation result based on the given operator

--- app.js
+++ app.js
@@ -13,3 +13,15 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
+// Returns the result of the operation designated by the operator passed as parameter (String)
+function operate(operator, firstNum, secondNum) {
+  const operations = {
+    "+": add(firstNum, secondNum),
+    "-": subtract(firstNum, secondNum),
+    "*": multiply(firstNum, secondNum),
+    "/": divide(firstNum, secondNum),
+  };
+
+  return operations[operator];
+}
